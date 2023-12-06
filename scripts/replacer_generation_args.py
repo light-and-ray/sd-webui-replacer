b72c3f88d3139c7cf2c256147b7600601fb4b92c
Andray <light.and.ray@gmail.com>
2023-12-06 22:51:20 +0400

    Merge PR #1 form w-e-w

--- scripts/replacer_generation_args.py
+++ scripts/replacer_generation_args.py
@@ -1,54 +1,29 @@
+from dataclasses import dataclass
+from typing import Any
 
+@dataclass
 class GenerationArgs:
-    def __init__(self,
-            positvePrompt,
-            negativePrompt,
-            detectionPrompt,
-            mask,
-            upscalerForImg2Img,
-            seed,
-            samModel,
-            grdinoModel,
-            boxThreshold,
-            maskExpand,
-            steps,
-            sampler_name,
-            mask_blur,
-            inpainting_fill,
-            n_iter,
-            batch_size,
-            cfg_scale,
-            denoising_strength,
-            height,
-            width,
-            inpaint_full_res_padding,
-            img2img_fix_steps,
-            images,
-            generationsN,
-        ):
-        self.positvePrompt = positvePrompt
-        self.negativePrompt = negativePrompt
-        self.detectionPrompt = detectionPrompt
-        self.mask = mask
-        self.upscalerForImg2Img = upscalerForImg2Img
-        self.seed = seed
-        self.samModel = samModel
-        self.grdinoModel = grdinoModel
-        self.boxThreshold = boxThreshold
-        self.maskExpand = maskExpand
-        
-        self.steps = steps
-        self.sampler_name = sampler_name
-        self.mask_blur = mask_blur
-        self.inpainting_fill = inpainting_fill
-        self.n_iter = n_iter
-        self.batch_size = batch_size
-        self.cfg_scale = cfg_scale
-        self.denoising_strength = denoising_strength
-        self.height = height
-        self.width = width
-        self.inpaint_full_res_padding = inpaint_full_res_padding
-        self.img2img_fix_steps = img2img_fix_steps
-        
-        self.images = images
-        self.generationsN = generationsN
+    positvePrompt: Any
+    negativePrompt: Any
+    detectionPrompt: Any
+    mask: Any
+    upscalerForImg2Img: Any
+    seed: Any
+    samModel: Any
+    grdinoModel: Any
+    boxThreshold: Any
+    maskExpand: Any
+    steps: Any
+    sampler_name: Any
+    mask_blur: Any
+    inpainting_fill: Any
+    n_iter: Any
+    batch_size: Any
+    cfg_scale: Any
+    denoising_strength: Any
+    height: Any
+    width: Any
+    inpaint_full_res_padding: Any
+    img2img_fix_steps: Any
+    images: Any
+    generationsN: Any
