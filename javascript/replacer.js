cc66d737ca0da83a763924bb24609adedde85c68
Andray <light.and.ray@gmail.com>
2024-02-14 20:39:36 +0400

    fix #36

--- javascript/replacer.js
+++ javascript/replacer.js
@@ -62,7 +62,7 @@ function replacerGetCurrentSourceImg(dummy_component, isAvoid, needLimit, maxRes
 
 
 function replacerApplyZoomAndPanIntegration () {
-    if (typeof window.applyZoomAndPanIntegration === "function" && window.applyZoomAndPanIntegration_replacer_mod === "function") {
+    if (typeof window.applyZoomAndPanIntegration === "function" && typeof window.applyZoomAndPanIntegration_replacer_mod === "function") {
         window.applyZoomAndPanIntegration_replacer_mod("#replacer_advanced_options", ["#replacer_avoidance_mask", "#replacer_custom_mask"]);
         var index = uiUpdateCallbacks.indexOf(replacerApplyZoomAndPanIntegration);
         if (index !== -1) {
